f3f59ac927a5d03de86931e6f3b37d03c8dcbe18
Matthew Cheverie <matthewjohncheverie@gmail.com>
2022-06-18 13:19:58 -0400

    Delete unnecessary files and add changes to html and css to add styling and form for the to do list

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App subject="Clarice" />, document.getElementById("root"));
-
+ReactDOM.render(<App subject="Clarice" />, document.getElementById('root'));
 
